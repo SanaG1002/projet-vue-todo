560bd23b0c40e26b40e6f8e4c56330dcd197fffa
SanaG1002 <92936779+SanaG1002@users.noreply.github.com>
2022-12-05 12:39:27 -0500

    Correction todos service API

--- src/services/todos-service.js
+++ src/services/todos-service.js
@@ -1,5 +1,6 @@
+
 class TodosService {
-    url = "https://vue-todo-a5b4cf.can.canonic.dev/api/todos";
+    url = "https://vue-todo-a5b4cf.can.canonic.dev/api/todos/_id";
     accessToken = "637aa7d3e0a20b0008881c3c-b5e5c8af-dec1-40c2-ae81-071a662e96cb"
 
     async getTodos() {
